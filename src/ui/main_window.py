7d9c8a1552ff572776e30a9f6b168dec94bf4a0c
Graeme Harrison <graeme.harrison@nutanapower.com>
2025-04-01 21:45:51 -0600

    assets

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -228,7 +228,7 @@ class PowerSystemSimulator(QMainWindow):
         window_geometry = self.geometry()
         
         x = (screen_geometry.width() - window_geometry.width()) // 2
-        y = (screen_geometry.height() - window_geometry.height()) // 2
+        y = (screen_geometry.height() - window_geometry.height()) // 2 - 50
         
         self.move(x, y)
     
