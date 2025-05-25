bcf5fadcdb7010d98a2e81bfe7cc08da70b9de86
Graeme Harrison <graeme.harrison@augurvc.com>
2025-05-24 21:13:06 -0600

    video title

--- src/ui/augur_title_screen.py
+++ src/ui/augur_title_screen.py
@@ -1,10 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QGraphicsView, QGraphicsScene
-from PyQt5.QtGui import QKeyEvent, QPixmap
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QGraphicsRectItem
+from PyQt5.QtGui import QKeyEvent, QPixmap, QBrush, QPen
 from PyQt5.QtCore import Qt, QTimer, pyqtSignal, QUrl, QSizeF
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QGraphicsVideoItem
-from PyQt5.QtWidgets import QGraphicsPixmapItem
 from src.utils.resource import resource_path
 
 class AugurTitleScreen(QWidget):
@@ -41,6 +40,7 @@ class AugurTitleScreen(QWidget):
         # Create video item and media player
         self.video_item = QGraphicsVideoItem()
         self.video_item.setSize(QSizeF(1600, 900))
+        self.video_item.setZValue(0)  # Bottom layer
         self.graphics_scene.addItem(self.video_item)
         
         self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
@@ -59,9 +59,17 @@ class AugurTitleScreen(QWidget):
             logo_x = (1600 - scaled_logo.width()) / 2  # Center horizontally
             logo_y = (900 - scaled_logo.height()) / 2  # Center vertically
             self.logo_item.setPos(logo_x, logo_y)
+            self.logo_item.setZValue(1)  # Middle layer
             
             # Add logo to scene (it will be on top of video)
             self.graphics_scene.addItem(self.logo_item)
+        
+        # Create and add black mask on top of everything
+        self.black_mask = QGraphicsRectItem(0, 0, 1600, 900)
+        self.black_mask.setBrush(QBrush(Qt.black))
+        self.black_mask.setPen(QPen(Qt.NoPen))  # No border
+        self.black_mask.setZValue(2)  # Top layer - above everything
+        self.graphics_scene.addItem(self.black_mask)
         
         # Set the window size to match the video size
         self.setFixedSize(1600, 900)
@@ -79,16 +87,37 @@ class AugurTitleScreen(QWidget):
         self.timer.setSingleShot(True)
         self.timer.setInterval(5000)  # 5 seconds
         self.timer.timeout.connect(self.auto_transition)
+        
+        # Setup fade timer for black mask
+        self.fade_timer = QTimer(self)
+        self.fade_timer.setInterval(10)  # Update every 10ms for smooth animation
+        self.fade_timer.timeout.connect(self.fade_step)
+        self.fade_opacity = 1.0  # Start fully opaque
     
     def showEvent(self, event):
         """Start the video and timer when the widget is shown"""
         super().showEvent(event)
         self.media_player.play()
         self.timer.start()
+        # Start the fade animation for the black mask
+        self.fade_timer.start()
+    
+    def fade_step(self):
+        """Handle one step of the fade animation"""
+        self.fade_opacity -= 0.005
+        
+        if self.fade_opacity <= 0:
+            # Animation complete - make fully transparent and stop timer
+            self.fade_opacity = 0.0
+            self.fade_timer.stop()
+        
+        # Apply the new opacity to the black mask
+        self.black_mask.setOpacity(self.fade_opacity)
     
     def auto_transition(self):
         """Automatically transition to the next screen after timer expires"""
         self.media_player.stop()
+        self.fade_timer.stop()  # Stop fade timer when transitioning
         self.transition_to_next.emit()
         self.close()
     
@@ -109,6 +138,7 @@ class AugurTitleScreen(QWidget):
             # Stop video and timer if a key is pressed
             self.media_player.stop()
             self.timer.stop()
+            self.fade_timer.stop()  # Stop fade timer when transitioning
             self.transition_to_next.emit()
             self.close()
         # Also transition on Escape or Space for user convenience
@@ -116,6 +146,7 @@ class AugurTitleScreen(QWidget):
             # Stop video and timer if a key is pressed
             self.media_player.stop()
             self.timer.stop()
+            self.fade_timer.stop()  # Stop fade timer when transitioning
             self.transition_to_next.emit()
             self.close()
         else:
